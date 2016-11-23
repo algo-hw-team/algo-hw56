c33aecfd81bbe6839c2aac1592b50c7a9e1fd659
kjiho <geomet123@gmail.com>
2016-11-23 15:29:28 +0900

    1123

--- src/core/NodeList.java
+++ src/core/NodeList.java
@@ -1,5 +1,24 @@
 package core;
 
-public class NodeList {
+import java.util.ArrayList;
 
+public class NodeList {
+	public ArrayList<Node> list;
+	public double[][] distance;
+	public NodeList(ArrayList<Node> list) {
+		this.list = list;
+		distance = new double[list.size()][list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				int node1 = list.get(i).getId();
+				int node2 = list.get(j).getId();
+				if (node1 == node2) {
+					distance[node1][node2] = 0;
+				} else {
+					distance[node1][node2] = Math.sqrt(Math.pow(list.get(i).x - list.get(j).x,2) + Math.pow(list.get(i).y - list.get(j).y,2));
+				}
+			}
+		}
+	}
+	
 }
